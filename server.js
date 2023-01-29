68909986cd329da4280e1c1259cbf324ea8a0de6
SanghaviKA <sanghavi258@gmail.com>
2023-01-29 22:16:40 +0530

    updated the organisation project

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-// const low = require('lowdb');
-// const FileSync = require('lowdb/adapters/FileSync');
-// const adapter = new FileSync('db.json');
-// const db = low(adapter);
-
-const express = require('express');
-const jsonServer = require('json-server');
+const express = require("express");
+const jsonServer = require("json-server");
 
 const app = express();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 app.use(jsonServer.defaults());
 app.use(router);
 
 app.listen(3001, () => {
-console.log('JSON Server is running');
-  });
-  
-
+  console.log("JSON Server is running");
+});
